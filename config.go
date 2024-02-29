c67465d6a232094f956c8d6efed91f9f8f2f15c9
Esly Lescano <eslylescano@gmail.com>
2024-02-29 19:59:43 +0100

    configure secret key

--- config.go
+++ config.go
@@ -6,7 +6,7 @@ var AwsRegion = getEnv("AWS_REGION", "eu-west-2")
 var S3ProductBucket = getEnv("S3_BUCKET", "ecomerce-test-esly")
 var ProductDynamoTable = getEnv("PRODUCT_DYNAMODB_TABLE", "products")
 var UserDynamoTable = getEnv("USER_DYNAMODB_TABLE", "users-ecomerce-test")
-var SecretKey = []byte("your-secret-key")
+var SecretKey = []byte(getEnv("SECRET_KEY", "users-ecomerce-test"))
 
 func getEnv(key, fallback string) string {
 	if value, ok := os.LookupEnv(key); ok {
